54b1080c955c4c621455d62aaf20ea495eaf3bcb
Micaela Yaque <myaque@fi.uba.ar>
2026-06-11 03:29:45 -0300

    refactor: - "aceptarAccion" cambia el nombre a "aceptarAccionNocturna" - hardcodeamos proteger() para que elija al segundo de la lista de jugadores, es algo que deberíamos cuando agreguemos la ui dejar que elija el jugador "desde afuera"
    Co-authored-by: Rodrigo Ladaga <rladaga@fi.uba.ar>
    Co-authored-by: Camila Anahi Wilverht Rohr <cwilverht@fi.uba.ar>
    Co-authored-by: Franco Alejandro Lezcano <flezcano@fi.uba.ar>

--- src/main/java/edu/fiuba/algo3/modelo/roles/Medico.java
+++ src/main/java/edu/fiuba/algo3/modelo/roles/Medico.java
@@ -3,7 +3,7 @@ package edu.fiuba.algo3.modelo.roles;
 import edu.fiuba.algo3.modelo.fases.TurnoNocturno;
 import edu.fiuba.algo3.modelo.jugadores.Jugador;
 
-import java.util.Collection;
+import java.util.List;
 
 public class Medico extends Rol {
 
@@ -13,13 +13,11 @@ public class Medico extends Rol {
     }
 
     @Override
-    public void aceptarAccion(Jugador propio, TurnoNocturno turno) {
+    public void aceptarAccionNocturna(Jugador propio, TurnoNocturno turno) {
         turno.pedirAccion(propio, this);
     }
 
-    public Jugador proteger(Jugador objetivo, Collection<Jugador> vivos) {
-        // En la Entrega 1 la proteccion es directa; la restriccion de noches
-        // consecutivas se incorpora en la Entrega 2.
-        return objetivo;
+    public Jugador proteger(List<Jugador> jugadores) {
+        return jugadores.get(1);
     }
 }
