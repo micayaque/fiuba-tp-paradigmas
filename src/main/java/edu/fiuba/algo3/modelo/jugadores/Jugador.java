f099b542b9e2a46518e44e4fcd8f06042715187d
Micaela Yaque <myaque@fi.uba.ar>
2026-06-11 03:02:50 -0300

    refactor: - eliminamos las excepciones sobre jugadores sin roles - eliminamos el constructor sin el parámetro de rol porque no deberiamos crear "jugadores con estado inválido" que no tienen roles - el getter del nombre() todavía no lo usamos (tampoco el atributo) - eliminamos asignarCarta() porque no deberíamos "cambiar" el rol que ya tiene un jugador desde el inicio - eliminamos tieneRol() porque compara con null y no deberiamos tener jugadores con roles null porque seria un estado inválido - modificamos el getter del rol() para que no lo compare con null
    Co-authored-by: Rodrigo Ladaga <rladaga@fi.uba.ar>
    Co-authored-by: Camila Anahi Wilverht Rohr <cwilverht@fi.uba.ar>
    Co-authored-by: Franco Alejandro Lezcano <flezcano@fi.uba.ar>

--- src/main/java/edu/fiuba/algo3/modelo/jugadores/Jugador.java
+++ src/main/java/edu/fiuba/algo3/modelo/jugadores/Jugador.java
@@ -1,7 +1,5 @@
 package edu.fiuba.algo3.modelo.jugadores;
 
-import edu.fiuba.algo3.modelo.excepciones.JugadorSinRol;
-import edu.fiuba.algo3.modelo.excepciones.JugadorYaTieneRol;
 import edu.fiuba.algo3.modelo.roles.Rol;
 
 public class Jugador {
@@ -10,49 +8,25 @@ public class Jugador {
     private Rol carta;
     private EstadoJugador estado = new Vivo();
 
-    public Jugador(String nombre) {
-        this.nombre = nombre;
-    }
-
     public Jugador(String nombre, Rol carta) {
         this.nombre = nombre;
         this.carta = carta;
     }
 
-    public String nombre() {
-        return nombre;
+    public Rol rol() {
+        return this.carta;
     }
 
-    public void asignarCarta(Rol carta) {
-        if (this.carta != null) {
-            throw new JugadorYaTieneRol(nombre + " ya tiene un rol asignado");
-        }
-        this.carta = carta;
-    }
-
-    public boolean tieneRol() {
-        return carta != null;
-    }
-
-    public Rol miRol() {
-        if (carta == null) {
-            throw new JugadorSinRol(nombre + " todavia no tiene un rol asignado");
-        }
-        return carta;
-    }
-
-    public boolean conoceElRolDe(Jugador otro) {
-        if (otro == this) {
-            return true;
-        }
-        return miRol().conoceElRolDe(otro);
+    public boolean conoceElRolDe(Jugador otroJugador, Rol otroRol) {
+        return otroJugador == this || this.rol().conoceElRolDe(otroRol);
     }
 
     public boolean estaVivo() {
-        return estado.estaVivo();
+        return this.estado.estaVivo();
     }
 
     public void eliminar() {
-        this.estado = estado.eliminar();
+        this.estado = this.estado.eliminar();
     }
+
 }
