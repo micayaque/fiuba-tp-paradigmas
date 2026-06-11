ae56422c52675a2fd4ed5c00034057c4ae18d835
Micaela Yaque <myaque@fi.uba.ar>
2026-06-11 04:23:44 -0300

    refactor: - eliminamos la parte de los votos en blanco porque no los utilizamos para la entrega 1
    Co-authored-by: Rodrigo Ladaga <rladaga@fi.uba.ar>
    Co-authored-by: Camila Anahi Wilverht Rohr <cwilverht@fi.uba.ar>
    Co-authored-by: Franco Alejandro Lezcano <flezcano@fi.uba.ar>

--- src/main/test/edu/fiuba/algo3/entrega_1/VotacionNocturnaTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/VotacionNocturnaTest.java
@@ -4,54 +4,26 @@ import edu.fiuba.algo3.modelo.jugadores.Jugador;
 import edu.fiuba.algo3.modelo.roles.Ciudadano;
 import edu.fiuba.algo3.modelo.votacion.VotacionNocturna;
 import edu.fiuba.algo3.modelo.votacion.Voto;
-import edu.fiuba.algo3.modelo.votacion.VotoEnBlanco;
-import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class VotacionNocturnaTest {
 
-    private Jugador jugador(String nombre) {
-        return new Jugador(nombre, new Ciudadano());
-    }
+   private Jugador jugador(String nombre) {
+       return new Jugador(nombre, new Ciudadano());
+   }
 
-    @Test
-    public void elRecuentoIgnoraLosVotosEnBlancoYDevuelveElMasVotado() {
-        Jugador victima = jugador("Victima");
-        Jugador mafioso1 = jugador("Mafioso 1");
-        Jugador mafioso2 = jugador("Mafioso 2");
-        Jugador ciudadano1 = jugador("Ciudadano 1");
-        Jugador ciudadano2 = jugador("Ciudadano 2");
+   @Test
+   public void ganaElJugadorConMasVotos() {
+       Jugador a = jugador("A");
+       Jugador b = jugador("B");
 
-        VotacionNocturna votacion = new VotacionNocturna();
-        votacion.registrarVoto(new Voto(mafioso1, victima));
-        votacion.registrarVoto(new Voto(mafioso2, victima));
-        votacion.registrarVoto(new VotoEnBlanco(ciudadano1));
-        votacion.registrarVoto(new VotoEnBlanco(ciudadano2));
+       VotacionNocturna votacion = new VotacionNocturna();
+       votacion.registrarVoto(new Voto(a));
+       votacion.registrarVoto(new Voto(a));
+       votacion.registrarVoto(new Voto(b));
 
-        assertEquals(victima, votacion.resultadoVotacion());
-    }
-
-    @Test
-    public void siTodosVotanEnBlancoNoHayVictima() {
-        VotacionNocturna votacion = new VotacionNocturna();
-        votacion.registrarVoto(new VotoEnBlanco(jugador("Ciudadano 1")));
-        votacion.registrarVoto(new VotoEnBlanco(jugador("Ciudadano 2")));
-
-        assertNull(votacion.resultadoVotacion());
-    }
-
-    @Test
-    public void ganaElJugadorConMasVotos() {
-        Jugador a = jugador("A");
-        Jugador b = jugador("B");
-
-        VotacionNocturna votacion = new VotacionNocturna();
-        votacion.registrarVoto(new Voto(jugador("Mafioso 1"), a));
-        votacion.registrarVoto(new Voto(jugador("Mafioso 2"), a));
-        votacion.registrarVoto(new Voto(jugador("Mafioso 3"), b));
-
-        assertEquals(a, votacion.resultadoVotacion());
-    }
+       assertEquals(a, votacion.resultadoVotacion());
+   }
 }
