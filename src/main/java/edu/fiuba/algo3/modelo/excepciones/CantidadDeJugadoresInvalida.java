c13909d971f9adf2509b11651716ebb525ddd497
Micaela Yaque <myaque@fi.uba.ar>
2026-06-11 02:27:45 -0300

    refactor: eliminamos la excepción CantidadDeJugadoresInvalida porque no es algo que tenga que verificar el Mazo sino el Moderador que aún no lo implementamos
    Co-authored-by: Rodrigo Ladaga <rladaga@fi.uba.ar>
    Co-authored-by: Camila Anahi Wilverht Rohr <cwilverht@fi.uba.ar>
    Co-authored-by: Franco Alejandro Lezcano <flezcano@fi.uba.ar>

--- src/main/java/edu/fiuba/algo3/modelo/excepciones/CantidadDeJugadoresInvalida.java
+++
@@ -1,8 +0,0 @@
-package edu.fiuba.algo3.modelo.excepciones;
-
-public class CantidadDeJugadoresInvalida extends RuntimeException {
-
-    public CantidadDeJugadoresInvalida(String mensaje) {
-        super(mensaje);
-    }
-}
