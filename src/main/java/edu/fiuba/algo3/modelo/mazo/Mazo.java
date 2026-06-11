3c89215992900d14ed8e4e2e510ebcd8c2a08ce4
Micaela Yaque <myaque@fi.uba.ar>
2026-06-11 03:10:03 -0300

    refactor: - eliminamos la validación de la cantidad de jugadores correcta para el juego y la asignación de cartas para que el Mazo sólo se encargue de generar las cartas de roles de forma aleatoria y cumplir con SRP
    Co-authored-by: Rodrigo Ladaga <rladaga@fi.uba.ar>
    Co-authored-by: Camila Anahi Wilverht Rohr <cwilverht@fi.uba.ar>
    Co-authored-by: Franco Alejandro Lezcano <flezcano@fi.uba.ar>

--- src/main/java/edu/fiuba/algo3/modelo/mazo/Mazo.java
+++ src/main/java/edu/fiuba/algo3/modelo/mazo/Mazo.java
@@ -1,14 +1,6 @@
 package edu.fiuba.algo3.modelo.mazo;
 
-import edu.fiuba.algo3.modelo.excepciones.CantidadDeJugadoresInvalida;
-import edu.fiuba.algo3.modelo.jugadores.Jugador;
-import edu.fiuba.algo3.modelo.roles.Ciudadano;
-import edu.fiuba.algo3.modelo.roles.Detective;
-import edu.fiuba.algo3.modelo.roles.Mafioso;
-import edu.fiuba.algo3.modelo.roles.Medico;
-import edu.fiuba.algo3.modelo.roles.Padrino;
-import edu.fiuba.algo3.modelo.roles.Rol;
-import edu.fiuba.algo3.modelo.roles.Sheriff;
+import edu.fiuba.algo3.modelo.roles.*;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -16,11 +8,7 @@ import java.util.List;
 
 public class Mazo {
 
-    private static final int MINIMO_JUGADORES = 5;
-    private static final int MAXIMO_JUGADORES = 12;
-
     public List<Rol> generarPara(int cantidadJugadores) {
-        validarCantidad(cantidadJugadores);
 
         int mafiososTotales = totalDeMafiosos(cantidadJugadores);
         int padrinos = cantidadJugadores >= 10 ? 1 : 0;
@@ -37,15 +25,8 @@ public class Mazo {
         agregar(roles, medicos, Medico::new);
         agregar(roles, sheriffs, Sheriff::new);
         agregar(roles, ciudadanos, Ciudadano::new);
-        return roles;
-    }
-
-    public void asignarCartas(List<Jugador> jugadores) {
-        List<Rol> roles = generarPara(jugadores.size());
         Collections.shuffle(roles);
-        for (int i = 0; i < jugadores.size(); i++) {
-            jugadores.get(i).asignarCarta(roles.get(i));
-        }
+        return roles;
     }
 
     private int totalDeMafiosos(int cantidadJugadores) {
@@ -64,10 +45,4 @@ public class Mazo {
         }
     }
 
-    private void validarCantidad(int cantidadJugadores) {
-        if (cantidadJugadores < MINIMO_JUGADORES || cantidadJugadores > MAXIMO_JUGADORES) {
-            throw new CantidadDeJugadoresInvalida(
-                    "La cantidad de jugadores debe estar entre " + MINIMO_JUGADORES + " y " + MAXIMO_JUGADORES);
-        }
-    }
 }
