bf4ad775a6388807df2ff32ae2a88f25493c3efa
Micaela Yaque <myaque@fi.uba.ar>
2026-06-11 02:51:46 -0300

    refactor: - huboEliminacion() no deberia guardarse como atributo del resultado sino resolverse con los datos de los atributos (victima y protegido) - el getter protegido() no es necesario aún y debemos usar tdd - jugadorEliminado() tampoco es necesario aún y no deberiamos retornar literalmente null
    Co-authored-by: Rodrigo Ladaga <rladaga@fi.uba.ar>
    Co-authored-by: Camila Anahi Wilverht Rohr <cwilverht@fi.uba.ar>
    Co-authored-by: Franco Alejandro Lezcano <flezcano@fi.uba.ar>

--- src/main/java/edu/fiuba/algo3/modelo/fases/ResultadoNocturno.java
+++ src/main/java/edu/fiuba/algo3/modelo/fases/ResultadoNocturno.java
@@ -6,28 +6,18 @@ public class ResultadoNocturno implements ResultadoFase {
 
     private final Jugador victimaElegida;
     private final Jugador protegido;
-    private final boolean huboEliminacion;
 
-    public ResultadoNocturno(Jugador victimaElegida, Jugador protegido, boolean huboEliminacion) {
+    public ResultadoNocturno(Jugador victimaElegida, Jugador protegido) {
         this.victimaElegida = victimaElegida;
         this.protegido = protegido;
-        this.huboEliminacion = huboEliminacion;
     }
 
     public Jugador victimaElegida() {
         return victimaElegida;
     }
 
-    public Jugador protegido() {
-        return protegido;
-    }
-
-    public Jugador jugadorEliminado() {
-        return huboEliminacion ? victimaElegida : null;
-    }
-
     @Override
     public boolean huboEliminacion() {
-        return huboEliminacion;
+        return !this.protegido.equals(victimaElegida);
     }
 }
